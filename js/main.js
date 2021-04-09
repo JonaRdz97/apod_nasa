595fe6186ff6471317b2c024abe71162ffc9c7cf
Jonathan <jonathan.rdzes@gmail.com>
2021-04-09 13:14:09 -0500

    Cambios en el footer y archivo js

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
-var date_now = luxon.DateTime.now().toFormat("yyyy-LL-dd"); //se obtine la fecha de hoy para manejar el api
-var calendar = document.getElementById('date'); // obtener el elemento input type date
-var apod_content = document.getElementsByClassName('content-apod')[0];
-var title = document.getElementsByClassName('title')[0];
-var date_info = document.getElementsByClassName('date-info')[0];
-var explanation = document.getElementsByClassName('explanation')[0];
-var button = document.getElementById('btn');
-var media; // usada para la imagen/vídeo de la consulta
-var fecha_elegida; //usada para que  no se eliga la misma fecha en la consulta
+var date_now = luxon.DateTime.now().toFormat("yyyy-LL-dd"), //se obtine la fecha de hoy para manejar el api
+    calendar = document.getElementById('date'), // obtener el elemento input type date
+    apod_content = document.getElementsByClassName('content-apod')[0],
+    title = document.getElementsByClassName('title')[0],
+    date_info = document.getElementsByClassName('date-info')[0],
+    explanation = document.getElementsByClassName('explanation')[0],
+    button = document.getElementById('btn'),
+    media, // usada para la imagen/vídeo de la consulta
+    fecha_elegida; //usada para que  no se eliga la misma fecha en la consulta
 calendar.value = date_now; // pone el valor del imput con la fecha actual
 calendar.max = date_now; //el maximo valor de la fecha del input
 
@@ -16,30 +16,37 @@ get_data_api(date_now);
 // se reraliza la consulta  a la API
 //promise
 function get_data_api(choose_date){
-    fetch('https://api.nasa.gov/planetary/apod?api_key=oOEXRlpeboIutRlzQ4A9HvtCNzXfzlZYI9RKhjDw&date='+choose_date)
-    .then(data => data.json()) // los datos se pasan a formato json
-    .then(nasa_data => { 
-        // validar si es una imagen o video
-        if (nasa_data.media_type === "image") {
-            media = document.createElement('img');
-            media.className = "media-img";
-        } else if(nasa_data.media_type === "video"){
-            media = document.createElement('iframe');
-            media.className = "media-video";
-            media.allowFullscreen = "true";
-        }
-
-        fecha_elegida = nasa_data.date;
-
-        title.textContent = nasa_data.title;
-        date_info.textContent = "Fecha: " + nasa_data.date;
-        explanation.textContent = nasa_data.explanation;
-        media.src = nasa_data.url;
-
-        //agregar elemento media al documento
-        apod_content.appendChild(media);
-        console.log(nasa_data)
-    })
+    try {
+        fetch('https://api.nasa.gov/planetary/apod?api_key=oOEXRlpeboIutRlzQ4A9HvtCNzXfzlZYI9RKhjDw&date='+choose_date)
+        .then(data => data.json()) // los datos se pasan a formato json
+        .then(nasa_data => { 
+            // validar si es una imagen o video
+            if (nasa_data.media_type === "image") {
+                media = document.createElement('img');
+                media.className = "media-img";
+            } else if(nasa_data.media_type === "video"){
+                media = document.createElement('iframe');
+                media.className = "media-video";
+                media.allowFullscreen = "true";
+            }
+        
+            fecha_elegida = nasa_data.date;
+        
+            title.textContent = nasa_data.title;
+            date_info.textContent = "Fecha: " + nasa_data.date;
+            explanation.textContent = nasa_data.explanation;
+            media.src = nasa_data.url;
+        
+            //agregar elemento media al documento
+            apod_content.appendChild(media);
+        })
+    } catch (error) {
+        Swal.fire({
+            title: "ERROR",
+            text: "Error en la consulta" + error,
+            icon: "error",
+        });
+    }
 }
 
 
